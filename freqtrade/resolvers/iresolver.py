a166f452cb969664c1fa2c42ce9daea7f0d9b5a5
Matthias <xmatthias@outlook.com>
2025-10-28 19:32:57 +0100

    chore: move comment to right place

--- freqtrade/resolvers/iresolver.py
+++ freqtrade/resolvers/iresolver.py
@@ -86,13 +86,13 @@ class IResolver:
              Tuple format: [Object, source]
         """
 
-        # Generate spec based on absolute path
-        # Pass object_name as first argument to have logging print a reasonable name.
         with PathModifier(module_path.parent):
             module_name = module_path.stem or ""
+            # Generate spec based on absolute path
+            # Pass object_name as first argument to have logging print a reasonable name.
             spec = importlib.util.spec_from_file_location(module_name, str(module_path))
             if not spec:
-                return iter([None])
+                return iter([])
 
             module = importlib.util.module_from_spec(spec)
             try:
